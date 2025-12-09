5faee36645a956132b89fc65fdc15ffa8cff79bc
Nikita Litvinenko <nikitalitvinenko28@gmail.com>
2025-12-09 08:58:42 +0200

    Update

--- content/database2/config.php
+++ content/database2/config.php
@@ -1,7 +1,7 @@
 <?php
     $servername = "d141131.mysql.zonevs.eu";
     $username = "d141131_nikita";
-    $password = "b3BlbnNzaC1rZXktdj";
+    $password = "";
     $dbname = "d141131_phpbaas";
     $connect = new mysqli($servername, $username, $password, $dbname);
     $connect->set_charset("utf8");
