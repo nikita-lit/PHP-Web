f7a79201a21b7ef7f5be1c4167e060907c4e6f84
nikita-lit <nikitalitvinenko28@gmail.com>
2026-01-05 13:05:14 +0200

    Add NaitaTabel function

--- content/valimised/functions.php
+++ content/valimised/functions.php
@@ -11,3 +11,24 @@
         header("Location: " . $_SERVER["PHP_SELF"].'?id='.$id); // aadressi puhastab päring ja jääb faili nimi
         $connect->close();
     }
+
+    function NaitaTabel()
+    {
+        global $connect;
+        $query = $connect->prepare("SELECT id, president, pilt, punktid, lisamisaeg, kommentaarid FROM valimised WHERE avalik=1");
+        $query->bind_result($id, $president, $pilt, $punktid, $lisamisaeg, $kommentaarid);
+        $query->execute();
+        while($query->fetch())
+        {
+            echo "<tr>";
+            echo "<td>{$id}</td>";
+            echo "<td>{$president}</td>";
+            echo "<td><img src=$pilt' alt='President pilt'></td>";
+            echo "<td>{$punktid}</td>";
+            echo "<td>{$lisamisaeg}</td>";
+            echo "<td><a href='?lisa1punkt={$id}'>+1 punkt</a></td>";
+            echo "<td><a href='?lisa-1punkt={$id}'>-1 punkt</a></td>";
+            echo "<td>".nl2br(htmlspecialchars($kommentaarid))."</td>";
+            echo '</tr>';
+        }
+    }
