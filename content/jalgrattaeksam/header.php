14ca94fe4f9abd8854a0aed4a5209a1004832866
Nikita Litvinenko <nikitalitvinenko28@gmail.com>
2026-01-12 15:33:57 +0200

    Add logo to header
    Inserted a bicycle logo image into the header.

--- content/jalgrattaeksam/header.php
+++ content/jalgrattaeksam/header.php
@@ -1,3 +1,4 @@
 <header>
+    <img src="https://uxwing.com/wp-content/themes/uxwing/download/transportation-automotive/bicycle-icon.png" alt="Jalgrattaeksami logo"/>
     <h1>Jalgrattaeksam</h1>
 </header>
