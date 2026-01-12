9e0d56b29264b9a305543356470f93c59d478fda
Nikita Litvinenko <nikitalitvinenko28@gmail.com>
2026-01-12 15:43:29 +0200

    Add validation and message to registration form
    Added validation to ensure both first and last name fields are filled before submission.

--- content/jalgrattaeksam/content/registreerimine.php
+++ content/jalgrattaeksam/content/registreerimine.php
@@ -1,27 +1,50 @@
 <?php  
     require_once("funktsioonid.php");
 
+    $message = "";
+
     if(isset($_POST["submit"]))
     { 
-        Registreeri($_POST['eesnimi'], $_POST['perekonnanimi']);
+        $eesnimi = trim($_POST['eesnimi']);
+        $perekonnanimi = trim($_POST['perekonnanimi']);
 
-        $lisatud = $_POST['eesnimi'].' '.$_POST['perekonnanimi'];
-        header("Location: $_SERVER[PHP_SELF]?link=teooriaeksam.php&lisatudeesnimi=$lisatud");
-        exit();
+        if (!empty($eesnimi) && !empty($perekonnanimi))
+        {
+            Registreeri($eesnimi, $perekonnanimi);
+
+            $lisatud = $eesnimi.' '.$perekonnanimi;
+            header("Location: $_SERVER[PHP_SELF]?link=teooriaeksam.php&lisatudeesnimi=$lisatud");
+            exit();
+        }
+        else
+            $message = "Palun täida kõik väljad!";
     }
 ?>
 <div class="flex-container"> 
     <h1>Registreerimine</h1>
+
+    <?php
+        $display = "none";
+        if (!empty($message))
+            $display = "flex"; 
+
+        echo '<div id="lisati" style="display: '.$display.'">';
+    ?>
+        <?php 
+            if(!empty($message))
+                echo $message;
+        ?>
+    </div>
     
     <form action="?" method="post" id="registreerimine_form"> 
         <label>
             Eesnimi:
-            <input type="text" name="eesnimi"/>
+            <input type="text" name="eesnimi" required/>
         </label>
 
         <label>
             Perekonnanimi:
-            <input type="text" name="perekonnanimi"/>
+            <input type="text" name="perekonnanimi" required/>
         </label>
 
         <input type="submit" name="submit" value="Sisesta"/>
